58706e28d528e22b71335dcb06a8ed3a71a41779
Dhruv-malik <dhr456@gmail.com>
2019-11-28 16:30:02 +0530

    Update gh_users.sql

--- models/base/gh_users.sql
+++ models/base/gh_users.sql
@@ -1,4 +1,8 @@
-
-with Lead_data1 as (
-  select * from Lead_data
+with events as (
+  select campaign_id,
+         email_id,
+         sent_date,
+         decode(first_opened_date,null,0,1) as opened,
+         decode(unsubscribed,null,0,1) as unsubscribed
+         from {{ref('event_email_summary')}}
   )
